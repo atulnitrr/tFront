1789884c9d8cfd10f6c64db0d4f4c433f4b9f714
Atul Kumar <atul.kumar3@go-mmt.com>
2020-08-05 18:31:01 +0530

    Added commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Login.css";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, withRouter } from "react-router-dom";
 
-function Login() {
+const PATH = "http://localhost:3033";
+
+function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitCount, setSubmitCoount] = useState(0);
+
+  useEffect(() => {
+    async function login() {
+      try {
+        const userDetail = {
+          email_id: email,
+          password: password,
+        };
+        const response = await axios.post(`${PATH}/login`, userDetail);
+        if (response.status == 200) {
+          console.log(response.data);
+          props.history.push("/");
+        }
+      } catch (error) {}
+    }
+
+    if (submitCount > 0) {
+      login();
+    }
+  }, [submitCount]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitCoount((prev) => prev + 1);
+  };
+
   return (
     <div className="container">
       <div className="login-f-wrapper-top">
         <div className="login-f-wrapper">
           <span>Welcome, Please LogIn</span>
-          <form action="" className="login-form-wrapper">
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
+          <form
+            action=""
+            className="login-form-wrapper"
+            onSubmit={handleSubmit}
+            autoComplete="true"
+          >
+            <input
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value.trim())}
+              required
+              minLength="5"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value.trim())}
+              required
+              minLength="5"
+            />
             <button className="form-btn" type="submit">
               LogIn
             </button>
@@ -25,4 +76,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default withRouter(Login);
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,21 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 import "../css/SignUp.css";
 
-function SignUp() {
+const PATH = "http://localhost:3033";
+
+function SignUp(props) {
+  const [registerCount, setRegisterCount] = useState(0);
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    async function register() {
+      try {
+        const userDetail = {
+          first_name: first_name,
+          last_name: last_name,
+          email_id: email,
+          password: password,
+        };
+        const response = await axios.post(`${PATH}/signUp`, userDetail);
+        if (response.status == 200) {
+          console.log(response.data);
+          console.log(props.history);
+          props.history.push("/login");
+        }
+      } catch (error) {}
+    }
+    if (registerCount > 0) {
+      register();
+    }
+  }, [registerCount]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setRegisterCount((prev) => prev + 1);
+  };
   return (
     <div className="container">
       <div className="sign-up-f-wrapper-top">
         <div className="sign-up-f-wrapper">
           <span>Welcome, Please sign up</span>
-          <form action="" className="signup-form-wrapper">
-            <input type="first name" placeholder="first name" />
-            <input type="last name" placeholder="second name" />
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <input type="confirm password" placeholder="confirm password" />
+          <form
+            action=""
+            className="signup-form-wrapper"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              placeholder="first name"
+              value={first_name}
+              onChange={(e) => setFirstName(e.target.value.trim())}
+              required
+            />
+            <input
+              type="text"
+              placeholder="last name"
+              value={last_name}
+              onChange={(e) => setLastName(e.target.value.trim())}
+              required
+            />
+            <input
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value.trim())}
+              required
+              minLength="5"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value.trim())}
+              required
+              minLength="5"
+            />
+            <input
+              type="password"
+              placeholder="confirm password"
+              minLength="5"
+              required
+            />
             <button className="form-btn" type="submit">
               Sign Up
             </button>
@@ -30,4 +98,4 @@ function SignUp() {
   );
 }
 
-export default SignUp;
+export default withRouter(SignUp);
