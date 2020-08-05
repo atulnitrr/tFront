dfbd063718c8749e15938da8dd4115695904e55b
Atul Kumar <atul.kumar3@go-mmt.com>
2020-08-05 15:42:05 +0530

    Added sign up form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "../css/Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <h3>login</h3>
+    <div className="container">
+      <div className="login-f-wrapper-top">
+        <div className="login-f-wrapper">
+          <span>Welcome, Please LogIn</span>
+          <form action="" className="login-form-wrapper">
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button className="form-btn" type="submit">
+              LogIn
+            </button>
+          </form>
+          <div className="already-a-mem-login mem-b">
+            Not a member?
+            <Link to="/signup">SignUp</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,31 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+
+import "../css/SignUp.css";
 
 function SignUp() {
   return (
-    <div>
-      <h3>SignUp</h3>
+    <div className="container">
+      <div className="sign-up-f-wrapper-top">
+        <div className="sign-up-f-wrapper">
+          <span>Welcome, Please sign up</span>
+          <form action="" className="signup-form-wrapper">
+            <input type="first name" placeholder="first name" />
+            <input type="last name" placeholder="second name" />
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <input type="confirm password" placeholder="confirm password" />
+            <button className="form-btn" type="submit">
+              Sign Up
+            </button>
+          </form>
+          <div className="already-a-mem mem-b">
+            Already a member?
+            <Link to="/login">LogIn</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
